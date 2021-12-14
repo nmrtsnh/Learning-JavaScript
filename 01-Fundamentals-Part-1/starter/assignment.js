9201faf3a517743f4602b1276bc5bcd0d8a67717
Namrata Sinha <namratasinha26@gmail.com>
2021-12-14 17:48:11 +0100

    complete challenge on equality

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -47,8 +47,22 @@ if (population > averagePopulation) {
 5 + 6 + '4' + 9 - 4 - 2; = 1143
 */
 
+/*
 console.log("9" - "5");
 console.log("19" - "13" + "17");
 console.log("19" - "13" + 17);
 console.log("123" < 57);
 console.log(5 + 6 + "4" + 9 - 4 - 2);
+*/
+
+const numNeighbours = Number(
+  prompt("How many neighbour countries does your country have?")
+);
+
+if (numNeighbours === 1) {
+  console.log("Only 1 border!");
+} else if (numNeighbours > 1) {
+  console.log("More than 1 border");
+} else {
+  console.log("No borders");
+}
