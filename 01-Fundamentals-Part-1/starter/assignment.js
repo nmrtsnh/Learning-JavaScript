132920ab7ca99ecf77dd8fe369a1cc534835a378
Namrata Sinha <namratasinha26@gmail.com>
2021-12-14 12:59:28 +0100

    Complete assignment 2

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -8,4 +8,11 @@ console.log(country);
 console.log(continent);
 console.log(population);
 
-//
+// Data Types
+
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
