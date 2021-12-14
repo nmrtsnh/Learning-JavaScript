d96eb66c5cc85fc5127b925470be9c3c3a60ee92
Namrata Sinha <namratasinha26@gmail.com>
2021-12-14 15:43:50 +0100

    complete Basic Operator Assignments

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -3,16 +3,42 @@
 const country = "Germany";
 const continent = "Europe";
 let population = 83.24;
+const isIsland = false;
+language = "German";
+const description =
+  country +
+  " " +
+  "is " +
+  "in " +
+  continent +
+  "," +
+  " " +
+  "and " +
+  "its " +
+  population +
+  " " +
+  "million " +
+  "people " +
+  "speak " +
+  language;
+console.log(description);
+
+console.log(population / 2);
+population++;
+console.log(population / 2);
 population = 82;
+
+let FinlandPopulation = 6;
+let averagePopulation = 33;
+
+console.log(population > FinlandPopulation);
+console.log(population < averagePopulation);
 
 console.log(country);
 console.log(continent);
 console.log(population);
 
 // Data Types
-
-const isIsland = false;
-language = "German";
 
 console.log(typeof isIsland);
 console.log(typeof population);
