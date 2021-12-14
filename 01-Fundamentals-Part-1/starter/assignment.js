a56547b3fe30d3f74c0d4beb3c820e2e9fd9140b
Namrata Sinha <namratasinha26@gmail.com>
2021-12-14 18:35:10 +0100

    complete coding challenge 3

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -69,8 +69,10 @@ if (numNeighbours === 1) {
 }
 */
 
+/*
 if (language === "English" && population < 50 && !isIsland) {
   console.log(`You should live in ${country}`);
 } else {
   console.log(`${country} does not meet your criteria`);
 }
+*/
