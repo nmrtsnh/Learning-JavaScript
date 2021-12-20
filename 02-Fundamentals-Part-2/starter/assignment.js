5574546847d0f15b8761b740c3a634d26b1490b9
Namrata Sinha <namratasinha26@gmail.com>
2021-12-20 12:29:24 +0100

    update assignment

--- 02-Fundamentals-Part-2/starter/assignment.js
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -10,3 +10,29 @@ console.log(descGermany);
 
 const descFrance = describeCountry("France", 67, "Paris");
 console.log(descFrance);
+
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+
+const percIndia = percentageOfWorld1(1393);
+console.log(percIndia);
+
+const percGermany = percentageOfWorld1(89);
+console.log(percGermany);
+
+const percFrance = percentageOfWorld1(67);
+console.log(percFrance);
+
+const percentageOfWorld2 = function (country, population) {
+  return (population / 7900) * 100;
+};
+
+const percIndia1 = percentageOfWorld1(1393);
+console.log(percIndia1);
+
+const percGermany1 = percentageOfWorld1(89);
+console.log(percGermany1);
+
+const percFrance1 = percentageOfWorld1(67);
+console.log(percFrance1);
