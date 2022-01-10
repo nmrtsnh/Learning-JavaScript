da9b667ac943d07cc0ec3349b2fabf5c2260d7c7
Namrata Sinha <namratasinha26@gmail.com>
2022-01-10 15:37:07 +0100

    complete guess my number game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -11,34 +11,90 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
-
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
+let highscore = 0;
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+const displayNumber = function (number) {
+  document.querySelector('.number').textContent = number;
+};
+const displayScore = function (score) {
+  document.querySelector('.score').textContent = score;
+};
+const displayBody = function (body) {
+  document.querySelector('body').style.backgroundColor = body;
+};
+const styleNumber = function (style) {
+  document.querySelector('.number').style.width = style;
+};
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
 
+  //When there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔️ No Number';
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Too High!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '💥 You lost the game!';
-      document.querySelector('.score').textContent = 0;
-    }
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📉 Too Low!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '💥 You lost the game!';
-      document.querySelector('.score').textContent = 0;
+    displayMessage('⛔️ No Number');
+
+    //When player wins
+  } else if (guess === secretNumber) {
+    displayMessage('💫 Correct Number');
+    displayBody('#60b347');
+    styleNumber('30rem');
+
+    displayNumber(secretNumber);
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
     }
   }
+
+  //When the number is too high
+  else if (guess !== secretNumber) {
+    if (score > 1) {
+      //   document.querySelector('.message').textContent =
+      //     guess > secretNumber ? '📈 Too High!' : '📉 Too Low!';
+      displayMessage(guess > secretNumber ? '📈 Too High!' : '📉 Too Low!');
+      score--;
+      displayScore(score);
+    } else {
+      displayMessage('💥 You lost the game!');
+      displayScore(0);
+    }
+  }
+  //   else if (guess > secretNumber) {
+  //     if (score > 1) {
+  //       document.querySelector('.message').textContent = '📈 Too High!';
+  //       score--;
+  //       document.querySelector('.score').textContent = score;
+  //     } else {
+  //       document.querySelector('.message').textContent = '💥 You lost the game!';
+  //       document.querySelector('.score').textContent = 0;
+  //     }
+
+  //     //When the number is too low
+  //   } else if (guess < secretNumber) {
+  //     if (score > 1) {
+  //       document.querySelector('.message').textContent = '📉 Too Low!';
+  //       score--;
+  //       document.querySelector('.score').textContent = score;
+  //     } else {
+  //       document.querySelector('.message').textContent = '💥 You lost the game!';
+  //       document.querySelector('.score').textContent = 0;
+  //     }
+  //   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  displayMessage('Start guessing...');
+  displayNumber('?');
+  document.querySelector('.guess').value = '';
+  displayScore(score);
+
+  displayBody('#222');
+  styleNumber('15rem');
 });
