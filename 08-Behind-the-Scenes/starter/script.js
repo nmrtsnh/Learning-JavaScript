e5f8ce6cbcaa13a53830d7f08ae8d5f24ea88428
Namrata Sinha <namratasinha26@gmail.com>
2022-01-18 16:50:27 +0100

    update

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+
+      //// Creating new variable with same name as outer scope's variable
+      const firstName = 'Steven';
+
+      //Reassigning outer scope's variable
+      const output = 'NEW OUTPUT';
+
+      const str = `You're millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // console.log(str);
+    console.log(millenial); //// Var variables do not care about blocks.
+    // console.log(add(2, 3)); /// Only works without strict mode.
+    console.log(output);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
+// console.log(age);
+// printAge();
