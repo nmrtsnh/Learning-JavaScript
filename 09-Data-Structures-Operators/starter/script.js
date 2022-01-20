e588759821a3c7bd972bcacd90a876dcbb5df876
Namrata Sinha <namratasinha26@gmail.com>
2022-01-20 13:26:41 +0100

    update

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -47,7 +47,55 @@ const restaurant = {
       `Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`
     );
   },
+  orderPizza: function (mainIngredients, ...otherIngredients) {
+    console.log(mainIngredients);
+    console.log(otherIngredients);
+  },
 };
+
+// 1. Destructuring
+
+// Spread, because on RIGHT side of =
+const arr = [1, 2, ...[3, 4]];
+
+// Rest, because on left side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+// Objects
+
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+// 2. Functions
+
+const add = function (...numbers) {
+  console.log(numbers);
+  let sum = 0;
+
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+add(2, 3);
+add(1, 3, 4, 5);
+add(3, 4, 5, 2, 5, 3, 5, 3, 5, 4);
+
+const x = [23, 7, 6];
+add(...x);
+
+restaurant.orderPizza('mushroom', 'chicken', 'olives', 'spinach');
+restaurant.orderPizza('mushroom');
+
+/*
+
+////////////////////////////////
+//// The Spread Operator (...)
 
 const arr = [7, 8, 9];
 const badNewArr = [1, 2, 3, arr[0], arr[1], arr[2]];
@@ -96,6 +144,8 @@ const restaurantCopy = { ...restaurant };
 restaurantCopy.name = 'Ristorante Roma';
 console.log(restaurantCopy.name);
 console.log(restaurant.name);
+
+*/
 
 ////////////////////////////////////
 /*
