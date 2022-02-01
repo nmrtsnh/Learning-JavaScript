faf9391150b2a820312841ac13790627242ddbfc
Namrata Sinha <namratasinha26@gmail.com>
2022-02-01 12:28:47 +0100

    update

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,28 +1,12 @@
 'use strict';
+
 /*
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-*/
+  */
 
 // Data needed for first part of the section
-
-const weekdays = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
-const openingHours = {
-  [weekdays[4]]: {
-    open: 12,
-    close: 22,
-  },
-  [weekdays[5]]: {
-    open: 11,
-    close: 23,
-  },
-  [weekdays[6]]: {
-    open: 0, // Open 24 hours
-    close: 24,
-  },
-};
-
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -30,456 +14,35 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  /// ES6 enhanced object literals
-  openingHours,
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
 
-  /// In ES6, we can remove function or we can keep it
-  order(startingIndex, mainIndex) {
-    return [this.starterMenu[startingIndex], this.mainMenu[mainIndex]];
-  },
-  orderDelivery({ startingIndex = 1, mainIndex = 0, time = '20:00', address }) {
-    console.log(
-      `Order Receivd! ${this.starterMenu[startingIndex]} and ${this.mainMenu[mainIndex]} will be deliverd to ${address} at ${time}`
-    );
-  },
-  orderPasta: function (ing1, ing2, ing3) {
-    console.log(
-      `Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`
-    );
-  },
-  orderPizza: function (mainIngredients, ...otherIngredients) {
-    // console.log(mainIngredients);
-    // console.log(otherIngredients);
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 };
 
 /*
 /////////////////////////////////
-// Map Iteration
-
-const question = new Map([
-  ['question', 'what is your favorite programming language in the world?'],
-  [1, 'C'],
-  [2, 'Java'],
-  [3, 'JavaScript'],
-  ['correct', 3],
-  [true, 'Correct 🎉'],
-  [false, 'Try Again'],
-]);
-console.log(question);
-
-// Convert object to map
-console.log(Object.entries(openingHours));
-const hoursMap = new Map(Object.entries(openingHours));
-console.log(hoursMap);
-
-// Quiz app
-console.log(question.get('question'));
-for (const [key, value] of question) {
-  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
-}
-
-// const answer = Number(prompt('Your answer'));
-const answer = 3;
-console.log(answer);
-
-console.log(question.get(question.get('correct') === answer));
-
-// convert map to array
-
-console.log([...question]);
-console.log([...question.entries()]);
-console.log([...question.keys()]);
-console.log([...question.values()]);
-*/
-
-/*
-
-/////////////////////////////////////////////
-// Maps Fundamentals
-
-const rest = new Map();
-rest.set('name', 'Classico Italiano');
-rest.set(1, 'Frienze, Italy');
-console.log(rest.set(2, 'Lisbon, Portugal'));
-
-rest
-  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
-  .set('open', 11)
-  .set('close', 23)
-  .set(true, 'We are open :)')
-  .set(false, 'We are closed :(');
-
-console.log(rest.get('name'));
-console.log(rest.get(1));
-console.log(rest.get(2));
-console.log(rest.get(true));
-console.log(rest.get(false));
-console.log(rest.get('categories'));
-
-const time = 12;
-
-console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
-
-console.log(rest.has('categories'));
-rest.delete(2);
-// rest.clear();
-
-const arr = [1, 2];
-rest.set(arr, 'Test');
-console.log(rest);
-console.log(rest.size);
-
-console.log(rest.get(arr));
-*/
-
-/*
-//////////////////////////////
-
-/// SETS
-const orderSet = new Set(['Pizza', 'Pasta', 'Pizza', 'Risotto', 'Pasta']);
-console.log(orderSet);
-
-console.log(new Set('Namrata'));
-console.log(new Set());
-console.log(orderSet.size);
-console.log(orderSet.has('Pizza'));
-console.log(orderSet.has('Garlic Bread'));
-
-orderSet.add('Garlic Bread');
-console.log(orderSet);
-orderSet.delete('Risotto');
-// orderSet.clear();
-console.log(orderSet);
-
-for (const order of orderSet) console.log(order);
-
-// Example
-
-const staff = ['Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
-const staffUnique = [...new Set(staff)];
-console.log(staffUnique);
-
-console.log(new Set('Chef', 'Waiter', 'Manager', 'Chef', 'Waiter').size);
-*/
-
-/*
-///////////////////////////////////
-//// Property Names
-
-const Property = Object.keys(openingHours);
-console.log(Property);
-let openStr = `We are open on ${Property.length} days: `;
-
-for (const day of Property) {
-  openStr += `${day}, `;
-}
-console.log(openStr);
-
-//// Property Values
-
-const values = Object.values(openingHours);
-console.log(values);
-
-// Entire Object
-
-const entries = Object.entries(openingHours);
-console.log(entries);
-
-// [key, value]
-for (const [day, { open, close }] of entries) {
-  console.log(`On ${day}, we are open at ${open}, close at ${close}`);
-}
-*/
-/*
-////////////////////////////////////////////////////
-// Optional Chaining
-// With optional chaining, if a certain property does not exist then undefined is returned immediately.
-
-if (restaurant.openingHours && restaurant.openingHours.mon)
-  console.log('restaurant.openingHours.mon.open');
-
-// With Optional Chaining
-console.log(restaurant.openingHours.mon?.open);
-console.log(restaurant.openingHours?.mon?.open);
-
-// Example
-
-const days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
-
-for (const day of days) {
-  const open = restaurant.openingHours[day]?.open ?? 'closed';
-
-  console.log(`On ${day}, we open at ${open} `);
-}
-
-// Methods
-console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
-console.log(restaurant.orderFocaccia?.(0, 1) ?? 'Method does not exist');
-
-// Arrays
-const users = [
-  {
-    name: 'Jonas',
-    email: 'hello@jonas.io',
-  },
-];
-// const users = [];
-console.log(users[0]?.name ?? 'users array empty');
-*/
-
-/*
-
-///////////////////////////////////
-
-//// The FOR Loop
-
-const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
-console.log(menu);
-
-for (const item of menu.entries()) console.log(item);
-
-for (const [i, el] of menu.entries()) {
-  console.log(`${i + 1}: ${el}`);
-}
-
-// console.log([...menu.entries()]);
-
-*/
-
-/*
-
-const rest1 = {
-  name: 'Capri',
-  numGuests: 20,
-};
-
-const rest2 = {
-  name: 'La Piazza',
-  owner: 'Giovanni Rossi',
-};
-
-// OR assignment operator
-// rest1.numGuests = rest1.numGuests || 10;
-// rest2.numGuests = rest2.numGuests || 10;
-
-// rest1.numGuests ||= 10;
-// rest2.numGuests ||= 10;
-// console.log(rest1);
-// console.log(rest2);
-
-// Nullish assignment operator ( Null or undefined)
-rest1.numGuests ??= 10;
-rest2.numGuests ??= 10;
-
-// AND assignment operator
-// rest1.owner = rest1.owner && '<ANONYMOUS>';
-// rest2.owner = rest2.owner && '<ANONYMOUS>';
-
-rest1.owner &&= '<ANONYMOUS>';
-rest2.owner &&= '<ANONYMOUS>';
-
-console.log(rest1);
-console.log(rest2);
-*/
-
-/*
-////////////////////////////
-// The Nullish Coalescing Operator
-
-restaurant.numGuests = 0;
-const guests = restaurant.numGuests || 10;
-console.log(guests);
-
-/// Nullish: null & undefined ( NOT 0 or "")
-const guestCorrect = restaurant.numGuests ?? 10;
-console.log(guestCorrect);
-*/
-
-/*
-/////////////////////////
-
-// Short circutting (&& and ||)
-// Three properties of logical operators: Use ANY data type, return ANY data type, short-circutting
-
-console.log('----- OR -----');
-console.log(3 || 'Jonas');
-console.log('' || 'Jonas');
-console.log(true || 0);
-console.log(undefined || null);
-
-console.log(undefined || 0 || '' || 'Jonas' || 23 || null);
-
-restaurant.numGuests = 23;
-const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
-console.log(guests1);
-
-const guests2 = restaurant.numGuests || 10;
-console.log(guests2);
-
-console.log('----- AND -----');
-console.log(0 && 'Jonas');
-console.log(2 && 'Jonas');
-
-/// Practical Example
-
-if (restaurant.orderPizza) {
-  restaurant.orderPizza('mushrooms', 'spinach');
-}
-
-restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
-
-*/
-
-/*
-
-////// Rest Pattern and Parameters
-
-// 1. Destructuring
-
-// Spread, because on RIGHT side of =
-const arr = [1, 2, ...[3, 4]];
-
-// Rest, because on left side of =
-const [a, b, ...others] = [1, 2, 3, 4, 5];
-console.log(a, b, others);
-
-const [pizza, risotto, ...otherFood] = [
-  ...restaurant.mainMenu,
-  ...restaurant.starterMenu,
-];
-console.log(pizza, risotto, otherFood);
-
-// Objects
-
-const { sat, ...weekdays } = restaurant.openingHours;
-console.log(weekdays);
-
-// 2. Functions
-
-const add = function (...numbers) {
-  console.log(numbers);
-  let sum = 0;
-
-  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
-  console.log(sum);
-};
-add(2, 3);
-add(1, 3, 4, 5);
-add(3, 4, 5, 2, 5, 3, 5, 3, 5, 4);
-
-const x = [23, 7, 6];
-add(...x);
-
-restaurant.orderPizza('mushroom', 'chicken', 'olives', 'spinach');
-restaurant.orderPizza('mushroom');
-
-//// The Spread and Rest syntax both looks exactly the same but they work in opposite ways depending on where they are used.
-// The spread operator used where we would otherwise write values seperated by a comma.
-// The Rest patter is basically used where we would otherwise write variables name seperated by commas
-*/
-
-/*
-
-////////////////////////////////
-//// The Spread Operator (...)
-
-const arr = [7, 8, 9];
-const badNewArr = [1, 2, 3, arr[0], arr[1], arr[2]];
-console.log(badNewArr);
-
-const newArr = [1, 2, 3, ...arr];
-console.log(newArr);
-
-console.log(...newArr);
-
-const newMenu = [...restaurant.mainMenu, 'Gnoochi'];
-console.log(newMenu);
-
-/// Copy array
-const mainMenuCopy = [...restaurant.mainMenu];
-console.log(mainMenuCopy);
-
-/// Join 2 arrays
-const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
-console.log(menu);
-
-//// Iterables: arrays, strings, maps, sets. NOT objects.
-const str = 'Jonas';
-const letters = [...str, ' ', 's'];
-console.log(letters);
-/// We can only use the spread operator when building an array or when we pass a value into a function.
-console.log(...str);
-// console.log(`${...str} Schmedtmann`); /// Multiple values seperated by a comma are usually only expected when we pass argument into a function or when we build a new array
-
-/// Real World Example
-const ingredients = [
-  // prompt("Let's make your pasta! Ingredients 1?"),
-  // prompt('Ingredients 2?'),
-  // prompt('Ingredients 3?'),
-];
-
-restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
-
-restaurant.orderPasta(...ingredients);
-
-//Objects
-const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Guiseppe' };
-console.log(newRestaurant);
-
-const restaurantCopy = { ...restaurant };
-restaurantCopy.name = 'Ristorante Roma';
-console.log(restaurantCopy.name);
-console.log(restaurant.name);
-*/
-
-////////////////////////////////////
-/*
-restaurant.orderDelivery({
-  time: '22:00',
-  address: 'Via Del Sol, 21',
-  startingIndex: 2,
-  mainIndex: 2,
-});
-
-restaurant.orderDelivery({ address: 'Via Del Sol, 21', startingIndex: 1 });
-
-const { name, openingHours, categories } = restaurant;
-console.log(name, openingHours, categories);
-
-const {
-  name: restaurantName,
-  openingHours: hours,
-  categories: tags,
-} = restaurant;
-console.log(restaurantName, hours, tags);
-
-//// Default values
-const { menu = [], starterMenu = (starters = []) } = restaurant;
-console.log(menu, starterMenu);
-
-//// Mutating values
-let a = 119;
-let b = 999;
-const obj = { a: 27, b: 30, c: 14 };
-({ a, b } = obj);
-console.log(a, b);
-
-//// Nested Objects
-const {
-  fri: { open: o, close: c },
-} = openingHours;
-console.log(o, c);
-*/
-
-/*
-/////////////////////////////
-
-//// Destructuring Arrays
-const arr = [1, 2, 3];
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
+/// Destructuring Arrays
+// For arrays we use destructuring to retrieve elements from the array and store them into variables in a very easy way.
+
+const arr = [2, 3, 4];
+const a = [0];
+const b = [1];
+const c = [2];
 
 const [x, y, z] = arr;
 console.log(x, y, z);
@@ -488,230 +51,26 @@ console.log(arr);
 let [main, , secondary] = restaurant.categories;
 console.log(main, secondary);
 
-// Switching variables
 // const temp = main;
 // main = secondary;
 // secondary = temp;
 // console.log(main, secondary);
-// console.log(temp);
 
 [main, secondary] = [secondary, main];
 console.log(main, secondary);
 
-/// Receive 2 return values from function
+// Receive 2 return values from a function
 const [starter, mainCourse] = restaurant.order(2, 0);
 console.log(starter, mainCourse);
 
-// Nested destructuring
+// Nested Destructuring
 const nested = [2, 4, [5, 6]];
 // const [i, , j] = nested;
 // console.log(i, j);
 const [i, , [j, k]] = nested;
 console.log(i, j, k);
 
-// Default values
-const [p = 1, q = 1, r = 1] = [8, 5];
+// Default Values
+const [p = 1, q = 1, r = 1] = [8, 9];
 console.log(p, q, r);
 */
-
-/*
-///////////////////////////////////////
-
-// Data Structures, Modern Operators and Strings
-
-// Coding Challenge #1
-
-We're building a football betting app (soccer for my American friends �)! Suppose we get data from a web service about a certain game ('game' variable on next page .In this challenge we're gonna work with that data.
-
-Your tasks:
-1. Create one player array for each team (variables 'players1' and 'players2')
-2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
-field players
-3. Create an array 'allPlayers' containing all players of both teams (22 players)
-4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
-5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
-6. Write a function ('printGoals') that receives an arbitrary number of player names (not an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
-7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, without using an if/else statement or the ternary operator.
-
-Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
-*/
-
-/*
-
-/// Solution
-
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
-
-// 1.
-const [players1, players2] = game.players;
-console.log(players1, players2);
-
-// 2.
-const [gk, ...fieldPlayers] = players1;
-console.log(gk, fieldPlayers);
-
-// 3.
-const allPlayers = [...players1, ...players2];
-console.log(allPlayers);
-
-// 4.
-const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
-console.log(players1Final);
-
-5;
-const {
-  odds: { team1, x: draw, team2 },
-} = game;
-console.log(team1, draw, team2);
-
-// 6.
-const printGoals = function (...players) {
-  console.log(players);
-  console.log(`${players.length} goals were scored`);
-};
-
-// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
-printGoals(...game.scored);
-
-// 7.
-team1 < team2 && console.log('Team 1 is more likely to win');
-team1 > team1 && console.log('Team 2 is more likely to win');
-*/
-
-/*
-///////////////////////////////////////////////
-// Coding Challenge #2
-
-Let's continue with our football betting app! Keep using the 'game' variable from before
-Your tasks:
-1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
-2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
-3. Print the 3 odds to the console, but in a nice formatted way, exactly like this: 
-Odd of victory Bayern Munich: 1.33 
-Odd of draw: 3.25
-Odd of victory Borrussia Dortmund: 6.5
-Get the team names directly from the game object, don't hardcode them(except for "draw"). Hint: Note how the odds and the game objects have the same property names �
-4. Bonus: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
-{
- Gnarby: 1,
- Hummels: 1,
- Lewandowski: 2
-}
-*/
-
-/*
-/// Solution
-// 1.
-const goals = [...game.scored];
-console.log(goals);
-
-for (const [i, player] of goals.entries()) {
-  // console.log(...game.scored);
-  console.log(`Goal ${i + 1}: ${player} `);
-}
-
-// 2.
-const odds = Object.values(game.odds);
-let average = 0;
-for (const odd of odds) average += odd;
-average /= odds.length;
-console.log(average);
-
-// 3.
-for (const [team, odd] of Object.entries(game.odds)) {
-  const teamStr = team === 'x' ? 'draw' : `Victory of ${game[team]}`;
-  console.log(`Odd of ${teamStr}: ${odd}`);
-}
-
-// 4.
-const scores = {};
-for (const player of game.scored) {
-  scores[player] ? scores[player]++ : (scores[player] = 1);
-}
-*/
-
-/*
-///////////////////////
-Coding Challenge #3
-Let's continue with our football betting app! This time, we have a map called 'gameEvents' (see below) with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
-
-Your tasks:
-1. Create an array 'events' of the different game events that happened (no duplicates)
-2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
-3. Compute and log the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
-4. Loop over 'gameEvents' and log each element to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
-[FIRST HALF] 17: ⚽ GOAL
-
-
-*/
-
-// Solution
-
-const gameEvents = new Map([
-  [17, '⚽ GOAL'],
-  [36, '� Substitution'],
-  [47, '⚽ GOAL'],
-  [61, '� Substitution'],
-  [64, '� Yellow card'],
-  [69, '� Red card'],
-  [70, '� Substitution'],
-  [72, '� Substitution'],
-  [76, '⚽ GOAL'],
-  [80, '⚽ GOAL'],
-  [92, '� Yellow card'],
-]);
-
-const events = [...new Set(gameEvents.values())];
-console.log(events);
-
-gameEvents.delete(64);
-
-const time = [...gameEvents.keys()].pop();
-console.log(time);
-console.log(
-  `An event happened, on average, every ${time / gameEvents.size} minutes`
-);
-
-for (const [min, event] of gameEvents) {
-  const half = min <= 45 ? 'FIRST' : 'SECOND';
-  console.log(`[${half} HALF] ${min}: ⚽️ ${event}`);
-}
