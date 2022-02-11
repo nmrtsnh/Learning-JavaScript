0ba8e571e6cbf80ffcd7b949cd6ee0bcb5c83f6a
Namrata Sinha <namratasinha26@gmail.com>
2022-02-11 17:14:37 +0100

    update

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -55,7 +55,83 @@ const restaurant = {
   },
 };
 
-// A map is a data structure that we can use to map values to keys.
+/*
+///////////////////////////
+// Map- Iteration
+
+const question = new Map([
+  ['question', 'What is the best programming language?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'Javascript'],
+  ['Correct', 3],
+  [true, 'Correct'],
+  [false, 'try again'],
+]);
+console.log(question);
+
+// Convert map to object
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Quiz app
+
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+
+// const answer = Number(prompt('Your answer'));
+const answer = 3;
+console.log(answer);
+
+console.log(question.get(question.get('Correct') === answer));
+
+// Convert map to array
+console.log(...question);
+console.log(question.entries());
+console.log(...question.keys());
+console.log(...question.values());
+*/
+
+/*
+////////////////////////////////////
+// Maps: Fundamentals
+// A map is a data structure that we can use to map values to keys. Just like an object, data is stored in key value pairs in maps. In maps, the keys can have any type like objects, arrays, or other maps. In objects the keys are always string.
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Freinze, Italy');
+rest.set(2, 'Lisbon, Portugal');
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :)')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(2));
+
+const time = 21;
+// Not readable. Don't use much
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+*/
+
 /*
 ///////////////////////////////////
 
