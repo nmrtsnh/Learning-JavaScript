f63921ed570d9b97ef3b9a578b2ad87112324fda
Namrata Sinha <namratasinha26@gmail.com>
2022-02-11 15:51:25 +0100

    update

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -7,6 +7,22 @@ const flights =
   */
 
 // Data needed for first part of the section
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -14,26 +30,14 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+  // ES6 enhanced object literals
+  openingHours,
 
-  order: function (starterIndex, mainIndex) {
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery: function ({ starterIndex = 0, mainIndex = 1, time, address }) {
+  orderDelivery({ starterIndex = 0, mainIndex = 1, time, address }) {
     console.log(
       `Order Received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
@@ -44,7 +48,411 @@ const restaurant = {
       `Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`
     );
   },
+
+  orderPizza: function (mainIngredients, ...otherIngredients) {
+    console.log(mainIngredients);
+    console.log(otherIngredients);
+  },
 };
+
+// A map is a data structure that we can use to map values to keys.
+/*
+///////////////////////////////////
+
+// Sets
+
+const orderSet = new Set([
+  'Pizza',
+  'Pasta',
+  'Pasta',
+  'Risotto',
+  'Pizza',
+  'Risotto',
+]);
+console.log(orderSet);
+
+console.log(new Set('Namrata'));
+
+// We can get the size of the set
+console.log(orderSet.size);
+
+// We can check if the certain element is in the set.
+console.log(orderSet.has('Pizza'));
+console.log(orderSet.has('Bread'));
+
+// We can also add new element to a set
+orderSet.add('Garlic Bread');
+orderSet.add('Garlic Bread');
+
+orderSet.delete('Risotto');
+// orderSet.clear();
+console.log(orderSet);
+// There is no need for getting data out of a set that's because all values are unique and if there order does not matter then there is no point of retreiving values out of a set.
+
+for (const order of orderSet) console.log(order);
+
+// The main use case of sets is to remove the duplicates values of arrays.
+
+// Example
+const staff = ['Manager', 'Waiter', 'Waiter', 'Chef', 'Chef'];
+
+const staffUnique = [...new Set(staff)];
+
+console.log(staffUnique);
+
+console.log(new Set(['Manager', 'Waiter', 'Waiter', 'Chef', 'Chef']).size);
+*/
+
+/* 
+
+//////////////////////////////////////////
+// Coding Challenge 2
+Let's continue with our football betting app! Keep using the 'game' variable from before.
+
+Your tasks:
+1. Loop over the game.scored array and print each player name to the console,
+along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already
+studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+Odd of victory Bayern Munich: 1.33
+Odd of draw: 3.25
+Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). Hint: Note how the odds and the game objects have the
+same property names �
+4. Bonus: Create an object called 'scorers' which contains the names of the
+players who scored as properties, and the number of goals as the value. In this
+game, it will look like this:
+{
+ Gnarby: 1,
+ Hummels: 1,
+ Lewandowski: 2
+}
+*/
+
+/*
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+// Solution
+for (const [num, player] of game.scored.entries()) {
+  console.log(`Goal ${num + 1}: ${player}`);
+}
+
+const odds = Object.values(game.odds);
+let avg = 0;
+for (const odd of odds) avg += odd;
+avg /= odds.length;
+
+for (const [team, odd] of Object.entries(game.odds)) {
+  // console.log(team, odd);
+  const teamStr = team === 'x' ? 'draw' : `Victory ${game[team]}`;
+  console.log(`Odd of ${teamStr}: ${odd}`);
+}
+*/
+
+/*
+/////////////////////////////////////////
+// Looping Objects: Object Keys, Values, and Entries
+// Property NAMES
+
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+console.log(openStr);
+
+// Property VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+//Entire Object
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+// [key, value]
+for (const [day, { open, close }] of entries) {
+  console.log(`On ${day}, we are open at ${open}, and close at ${close}`);
+}
+*/
+
+/*
+
+////////////////////////////////
+
+////// Optional Chaining
+
+
+if (restaurant.openingHours && restaurant.openingHours.mon) {
+  console.log(restaurant.openingHours.mon.open);
+}
+
+// console.log(restaurant.openingHours.mon.open);
+// With Optional Chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+//Example
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'Closed';
+  console.log(`On ${day}, we are open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+// Arrays
+const user = [{ name: 'namrata', email: 'hello@namrata.com' }];
+console.log(user[0]?.name ?? 'User does not exist');
+
+if (user.length > 0) {
+  console.log(user[0].name);
+} else {
+  console.log('User does not exist');
+}
+*/
+
+/*
+////////////////////////////////
+// The FOR-Off loop
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+for (const item of menu) console.log(item);
+
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`);
+}
+*/
+
+/*
+
+Coding Challenge #1
+We're building a football betting app (soccer for my American friends �)! Suppose we get data from a web service about a certain game ('game' variable on next page). In this challenge we're gonna work with that data.
+
+Your tasks:
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the
+goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
+field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus
+'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called
+'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player
+names (not an array) and prints each of them to the console, along with the
+number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which
+team is more likely to win, without using an if/else statement or the ternary
+operator. 
+
+Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'.
+Then, call the function again with players from game.scored
+*/
+/*
+// Solution
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+const printGoals = function (...players) {
+  // console.log(players);
+  console.log(`${players.length} goals were scored`);
+};
+
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals(...game.scored);
+
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
+*/
+
+/*
+////////////////////////////////
+// Logical Assignment Operator
+
+const rest1 = {
+  name: 'Capri',
+  // numGuests: 20,
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+
+// OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// nullish assignment operator (null of undefined)
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
+*/
+
+/*
+///////////////////////////////////////////
+// The Nullish Coalescing Operator
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
+
+// Nullish: Null and undefined (NOT 0 or "")
+const guestsCorrect = restaurant.numGuests ?? 10;
+console.log(guestsCorrect);
+*/
+
+/*
+/////////////////////////////////
+// Short Circuiting (&& and ||)
+console.log('-------- OR --------');
+
+// Logical operator can Use ANY data type, return ANY data type, short-circuiting
+// In the case of the OR operator, short-circuiting means that if the first value is a truthy value, it will immediately return that first value.
+
+console.log(3 || 'Namrata');
+console.log('' || 'Namrata');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+// restaurant.numGuests = 23;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('------- AND -------');
+// The AND Operator works exact opposite way
+console.log(0 && 'Namrata');
+console.log(7 && 'Namrata');
+
+console.log('Hello' && 23 && null && 'Namrata');
+
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('Mushroom', 'Spinach');
+}
+
+restaurant.orderPizza && restaurant.orderPizza('Mushroom', 'Spinach');
+
+// To summarize- The OR operator will return the first truthy value of all the operants or simply the last value if all of them are falsy. On the other hand the AND operator will return the first falsy value or the last value if all of them are truthy.
+// For practical applications, we can use the OR operator to set default values and we can use the AND operator to execute code in the second operant if the first one is true.
+*/
 
 /*
 ///////////////////////////////
@@ -100,6 +508,11 @@ console.log(restaurant.name);
 console.log(restaurantCopy.name);
 */
 
+/*
+
+////////////////////////
+// REST patterns and parameters
+
 // 1. Destructuring
 
 // SPREAD, because on RIGHT side of =
@@ -131,6 +544,10 @@ const add = function (...numbers) {
 add(2, 3);
 add(2, 4, 1, 7, 8);
 add(5, 6, 2, 9, 4, 3, 6, 7, 8, 9);
+
+restaurant.orderPizza('mushroom', 'spinach', 'olives', 'cheese');
+*/
+
 /*
 /////////////////////////
 // Destructuring Objects
