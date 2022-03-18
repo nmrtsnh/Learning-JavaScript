5621b8ea5b70f685e9aee22e8b7970ade96ac748
Namrata Sinha <namratasinha26@gmail.com>
2022-03-18 15:18:20 +0100

    complete coding challenge 1

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -104,16 +104,19 @@ console.log(arr.slice(2, -1));
 console.log(arr.slice(-1, 2));
 console.log(arr.slice());
 console.log([...arr]);
+*/
 
 // SPLICE Method
 // console.log(arr.splice(2));
-arr.splice(-1);
-console.log(arr);
-arr.splice(1, 2);
-console.log(arr);
-arr.splice(1);
-console.log(arr);
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// arr.splice(-1);
+// // console.log(arr);
+// arr.splice(1, 2);
+// console.log(arr);
+// arr.splice(1);
+// console.log(arr);
 
+/*
 // REVERSE Method
 arr = ['a', 'b', 'c', 'd', 'e'];
 const arr2 = ['j', 'i', 'h', 'g', 'f'];
@@ -185,4 +188,54 @@ console.log(currenciesUnique);
 currenciesUnique.forEach(function (value, _, map) {
   console.log(`${value}: ${value}`);
 });
+*/
+
+/*
+
+// Coding Challenge #1
+
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+Your tasks:
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Juliafoundoutthattheownersofthefirstandthelasttwodogsactuallyhave cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. CreateanarraywithbothJulia's(corrected)andKate'sdata
+3. Foreachremainingdog,logtotheconsolewhetherit'sanadult("Dognumber1
+is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy   ")
+4. Runthefunctionforbothtestdatasets
+
+Test data:
+§ Data 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3] § Data 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+Hints: Use tools from all lectures in this section so far 😉
+*/
+
+/*
+
+// Solutions
+
+const checkDogs = function (dogsJulia, dogsKate) {
+  const correctedDogsJulia = dogsJulia.slice();
+  correctedDogsJulia.splice(0, 1);
+  correctedDogsJulia.splice(-2);
+  // console.log(correctedDogsJulia);
+
+  // const dogs = [...correctedDogsJulia, ...dogsKate];
+  const dogs = correctedDogsJulia.concat(dogsKate);
+  // console.log(dogs);
+
+  dogs.forEach(function (dog, i) {
+    if (dog >= 3) {
+      console.log(
+        `Dog number ${i + 1} is an adult, and is ${dogs[i]} years old`
+      );
+    } else {
+      console.log(`Dog number ${i + 1} is still puppy`);
+    }
+  });
+};
+
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
 */
